35429ae6fb182b60e2a349901bf997fc85c582c9
cutiko <cutiko@gmail.com>
2017-12-06 12:53:43 -0300

    ADD: neark pokestops get

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.nearPokestop = functions.https.onRequest((request, response) => {
+    if (request.method === "GET") {
+        const uid = request.query.uid;
+        const latitude = request.query.latitude;
+        const longitude = request.query.longitude;
+        if (!uid || !latitude || !longitude) {
+            response.status(418).end();
+        } else {
+            const radius = 0.01;
+            const minLat = latitude - radius;
+            const maxLat = latitude - radius;
+            admin.firestore().collection('pokestops').where("latitude", ">=", minLat).where("latitude", "<=", maxLat).get().then(function (querySnapshot) {
+                var result = [];
+                querySnapshot.forEach(function (doc) {
+                    const pokestop = doc.data();
+                    result.push(pokestop);
+                });
+                response.status(200).send(result);
+            });
+        }
+    } else {
+        response.status(401).end();
+    }
+});
+
