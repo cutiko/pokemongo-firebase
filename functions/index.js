fe8397c7452f212a024522d7bb9b0d8e3b10e7bb
cutiko <cutiko@gmail.com>
2017-12-06 16:04:17 -0300

    REMOVE: boiler plate

--- functions/index.js
+++ functions/index.js
@@ -3,33 +3,6 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.nearPokestop = functions.https.onRequest((request, response) => {
-    if (request.method === "GET") {
-        const uid = request.query.uid;
-        const latitude = request.query.latitude;
-        const longitude = request.query.longitude;
-        if (!uid || !latitude || !longitude) {
-            response.status(418).end();
-        } else {
-            const radius = 0.005;
-            const parseLatitude = parseFloat(latitude);
-            const minLat = parseLatitude - radius;
-            const maxLat = parseLatitude + radius;
-            admin.firestore().collection('pokestops').where("latitude", ">=", minLat).where("latitude", "<=", maxLat).get().then(function (querySnapshot) {
-                var result = [];
-                const parseLongitude = parseFloat(longitude);
-                const minLongitude = parseLongitude - radius;
-                const maxLongitude = parseLongitude + radius;
-                querySnapshot.forEach(function (doc) {
-                    const pokestop = doc.data();
-                    if (pokestop.longitude >= minLongitude && pokestop.longitude <= maxLongitude) {
-                        result.push(pokestop);
-                    }
-                });
-                response.status(200).send(result);
-            });
-        }
-    } else {
-        response.status(401).end();
-    }
+    
 });
 
