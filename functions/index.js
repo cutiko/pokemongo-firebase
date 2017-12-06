bb3692213eb40ca7c29a78ea7adc344e0eb02e93
cutiko <cutiko@gmail.com>
2017-12-06 13:43:30 -0300

    FIX: results are precise

--- functions/index.js
+++ functions/index.js
@@ -10,14 +10,20 @@ exports.nearPokestop = functions.https.onRequest((request, response) => {
         if (!uid || !latitude || !longitude) {
             response.status(418).end();
         } else {
-            const radius = 0.01;
-            const minLat = latitude - radius;
-            const maxLat = latitude - radius;
+            const radius = 0.005;
+            const parseLatitude = parseFloat(latitude);
+            const minLat = parseLatitude - radius;
+            const maxLat = parseLatitude + radius;
             admin.firestore().collection('pokestops').where("latitude", ">=", minLat).where("latitude", "<=", maxLat).get().then(function (querySnapshot) {
                 var result = [];
+                const parseLongitude = parseFloat(longitude);
+                const minLongitude = parseLongitude - radius;
+                const maxLongitude = parseLongitude + radius;
                 querySnapshot.forEach(function (doc) {
                     const pokestop = doc.data();
-                    result.push(pokestop);
+                    if (pokestop.longitude >= minLongitude && pokestop.longitude <= maxLongitude) {
+                        result.push(pokestop);
+                    }
                 });
                 response.status(200).send(result);
             });
